da1a733119f95c02ccebb23cec1b8c19adb32352
fahiz7940 <mfahiz@99.com>
2025-08-22 15:52:06 +0530

    updated app.js

--- Basics/app.js
+++ Basics/app.js
@@ -63,3 +63,52 @@ if("hello"){
 if(0){
     console.log("wont run")
 }
+
+// --------🔹 Types of Operators in JavaScript --------
+
+// 1. Arithmetic Operators (Math operations)
+let num1 = 10, num2 =5
+console.log(num1 + num2) //Addition
+console.log(num1 - num2) //Subtraction
+console.log(num1 * num2) //Multiplication
+console.log(num1 / num2) //Division
+console.log(num1 % num2) // Modulus -> remider
+console.log(num1 ** num2) // Exponentiation -> x^y
+
+
+// 2. Assignment Operators
+
+let glass = 5
+glass += 2 //same as glass = glass + 2 -> 7
+glass -= 2 //same as glass = glass - 2 -> 5
+glass *= 3 //same as glass = glass * 3 -> 15
+glass /= 5 //same as glass = glass / 5 -> 3
+glass %= 2 //same as glass = glass % 2 -> 1
+glass **= 2 //same as glass = glass ** 2 -> 1
+
+ 
+// 3. Comparison Operators
+// (Used in conditions, return true or false.)
+let n1 = 5 , n2 = "5"
+
+console.log(n1 == n2) //(loose equality → value only)
+console.log(n1 === n2) //(strict equality → value + type)
+console.log(n1 != n2) 
+console.log(n1 !== n2)
+console.log(n1 > 3)
+console.log(n1 < 10)
+console.log(n1 >= 5)
+console.log(n1 <= 4)
+
+// 4. Logical Operators
+// Used to combine conditions.
+
+let g =true, f = false
+
+console.log(g && f) //AND 
+console.log(g || f) //OR
+console.log(!g) //NOT
+
+// 5. Unary Operators
+// Operate on a single value.
+
