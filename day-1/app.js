59a4d93b346cd41a7dad96a925288165ade5e26c
fahiz7940 <mfahiz99@gmail.com>
2025-08-29 20:19:34 +0530

    updated some files

---
+++ day-1/app.js
@@ -0,0 +1,23 @@
+let name = "fahiz"
+let role = 140
+let adult = true
+let emptyBox = null
+let empty;
+let maxint = 10000202371028912891127743854762n
+let syl = Symbol('unique')
+
+console.log(typeof(name))
+console.log(name)
+console.log(typeof(role))
+console.log(role)
+console.log(typeof(adult))
+console.log(adult)
+console.log(typeof(emptyBox))
+console.log(emptyBox)
+console.log(typeof(empty))
+console.log(empty)
+console.log(typeof(maxint))
+console.log(maxint)
+console.log(typeof(syl))
+console.log(syl)
+
